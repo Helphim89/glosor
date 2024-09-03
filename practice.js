5d6e3957ba159ddd6ec7839e787245f18d8857e1
Helphim89 <jonkkar@gmail.com>
2024-09-03 14:01:49 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -58,7 +58,7 @@ function checkAnswer() {
     // Visa knappen för att spela upp översättningen
     document.getElementById('play-foreign').style.display = 'inline-block';
 
-    setTimeout(loadNewWord, 3000); // Ladda ett nytt ord efter 3 sekunder
+    setTimeout(loadNewWord, 10000); // Ladda ett nytt ord efter 3 sekunder
 }
 
 function updateResultsList() {
